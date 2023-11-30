c6d54abc2e1910cd65ed8802273db563dfff4b41
Leo <leobado05gmail.com>
2023-11-30 16:30:23 +0100

    created axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/stocazzo", get(root2));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6969")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+async fn root2() -> &'static str {
+    "Hello, negus!"
 }
